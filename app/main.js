27a6fb0bda8f978cd1b46b7833917ba321cccc06
Niru Maheswaranathan <nirum@stanford.edu>
2013-08-24 18:22:46 -0700

    cell2json can add cell type information to json object

--- app/main.js
+++ app/main.js
@@ -16,29 +16,35 @@ function MainCtrl($scope) {
             alert(error);
          }
 
+				 // get cell type
+				 $scope.cellType = data.type;
+
          // plot data
          var svg = d3.select("svg");
          var w = 300;
          var h = 500;
-         var margin = 50;
+         var margin = 20;
          var rad = 1;
-         var xIdx = 2;
-         var yIdx = 1;
+         var xIdx = 1;
+         var yIdx = 0;
 
          // set up svg element
          svg.attr("width", w).attr("height", h).style("border", "2px solid red");
 
          // get extent of data
-         var x_extent = d3.extent(data.edges, function(d) {
-            return d.x[xIdx]
-         });
-         var y_extent = d3.extent(data.edges, function(d) {
-            return d.x[yIdx]
-         });
+         //var x_extent = d3.extent(data.edges, function(d) {
+            //return d.p1[xIdx]
+         //});
+         //var y_extent = d3.extent(data.edges, function(d) {
+            //return d.p2[yIdx]
+         //});
+				 extent = [ [30,130], [0,120], [0,80] ];
 
          // build the scales
-         var x = d3.scale.linear().range([margin, w - margin]).domain(x_extent);
-         var y = d3.scale.linear().range([h - margin, margin]).domain(y_extent);
+         //var x = d3.scale.linear().range([margin, w - margin]).domain(x_extent);
+         //var y = d3.scale.linear().range([h - margin, margin]).domain(y_extent);
+         var x = d3.scale.linear().range([margin, w - margin]).domain(extent[xIdx]);
+         var y = d3.scale.linear().range([margin, h - margin]).domain(extent[yIdx]);
 
          // make the axes
          // var xAxis = d3.svg.axis().scale(x).orient("bottom");
@@ -48,16 +54,16 @@ function MainCtrl($scope) {
 
          // add edges to svg
          svg.selectAll("line").data(data.edges).enter().append("line").attr("stroke-width", 1).attr("stroke", "red").attr("x1", function(d) {
-            return x(d.x[xIdx])
+            return x(d.p1[xIdx])
          })
          .attr("x2", function(d) {
-            return x(d.y[xIdx])
+            return x(d.p2[xIdx])
          })
          .attr("y1", function(d) {
-            return y(d.x[yIdx])
+            return y(d.p1[yIdx])
          })
          .attr("y2", function(d) {
-            return y(d.y[yIdx])
+            return y(d.p2[yIdx])
          });
       });
 
--- preprocess/cell2json.m
+++ preprocess/cell2json.m
@@ -8,32 +8,78 @@ clear; load('../raw/nature2013.mat');
 skeletons = kn_e2006_ALLSKELETONS_FINAL2012;
 cellIds   = kn_e2006_ALLSKELETONS_FINAL2012_cellIDs;
 matrixPos = kn_e2006_ALLSKELETONS_FINAL2012_cellIDs_sortedByType_MAR2013;
+typeId    = kn_e2006_ALLSKELETONS_FINAL2012_globalTypeIDs_REDOMAR2013;
 fprintf('Done.\n');
+
+%% store min/max of node values
+%nodeRange = [1e9*ones(1,3); -1*ones(1,3)];
+
+%%% store histogram of node locations
+%numBins = 100; bins = zeros(3,numBins);
+%bins(1,:) = linspace(0,131750,numBins);
+%bins(2,:) = linspace(0,119430,numBins);
+%bins(3,:) = linspace(0, 80025,numBins);
+%hists = zeros(length(matrixPos), numBins, 3);
+
+%% good ranges:
+%  x: 3e4 - 13e4 nm (30 - 130 mm)
+%  y: 0   - 12e4 nm ( 0 - 120 mm)
+%  z: 0   -  8e4 nm ( 0 -  80 mm)
+
+%% get cell type correspondences (using supp info 1 & 4)
+load('../raw/cellTypes.mat');
 
 %% Convert each cell's nodes and edges to a JSON object
 for matrixId = 1:length(matrixPos);
 
     % get cell ID based on matrix position
-		cellIdx = matrixPos(matrixId);
+	cellIdx = matrixPos(matrixId);
 
-		% get skeletons associated with this cell ID
-		skeletonIds = find(cellIds == cellIdx);
+	% get skeletons associated with this cell ID
+	skeletonIds = find(cellIds == cellIdx);
 
-		if isempty(skeletonIds)
-				continue;
-		else
-				skeletonId = skeletonIds(1); % pick one skeleton to use ...
-		end
+	if isempty(skeletonIds)
+		continue;
+	else
+		skeletonId = skeletonIds(1); % pick one skeleton to use ...
+	end
 
-    % nodes
-    %for nodeIdx = 1:size(skeletons{skeletonId}.nodes,1)
-        %obj.nodes{nodeIdx} = skeletons{skeletonId}.nodes(nodeIdx,1:3);
+    %% HISTOGRAMS
+    % get rough idea of the range of (connected) node values
+    skel = skeletons{skeletonId};
+    %nodes = skel.nodes(skel.edges(:),1:3);
+
+    %% check for min/max
+    %minVal = min(nodes); minIdx = min(nodes) < nodeRange(1,:); % min values & indices if novel
+    %maxVal = max(nodes); maxIdx = max(nodes) > nodeRange(2,:); % max values & indices if novel
+    %nodeRange(1,minIdx) = minVal(minIdx);
+    %nodeRange(2,maxIdx) = maxVal(maxIdx);
+
+    %% get histograms
+    %for j = 1:3
+        %hists(matrixId,:,j) = hist(nodes(:,j),bins(j,:));
     %end
 
+    %% get cell type
+    cellTypeIdx = find(cellTypeCorrespondence == typeId(skeletonId));
+    if cellTypeIdx <= 12
+        cellType = 'ganglion';
+    elseif (cellTypeIdx > 12) && (cellTypeIdx <= 24)
+        cellType = 'narrow-field amacrine';
+    elseif (cellTypeIdx > 24) && (cellTypeIdx <= 57)
+        cellType = 'medium/wide-field amacrine';
+    elseif (cellTypeIdx > 58) && (cellTypeIdx <= 71)
+        cellType = 'bipolar';
+    else
+        cellType = 'unknown';
+    end
+    obj.typeIdx = cellTypeIdx;
+    obj.type = cellType;
+
     % edges
-    for edgeIdx = 1:size(skeletons{skeletonId}.edges,1)
+    for edgeIdx = 1:size(skel.edges,1)
         edgeIndices = skeletons{skeletonId}.edges(edgeIdx,:);
-        obj.edges{edgeIdx} = struct('x', skeletons{skeletonId}.nodes(edgeIndices(1),1:3), 'y', skeletons{skeletonId}.nodes(edgeIndices(2),1:3));
+        obj.edges{edgeIdx} = struct('p1', skel.nodes(edgeIndices(1),1:3)/1e3, 'p2', skel.nodes(edgeIndices(2),1:3)/1e3);
     end
 
     % save to JSON
@@ -41,6 +87,6 @@ for matrixId = 1:length(matrixPos);
 
     % update
     %progressbar(id,length(ids));
-		tcprintf('lightGray onRed', 'Cell %i of %i.\n', matrixId, length(matrixPos));
+	tcprintf('lightGray onRed', 'Cell %i of %i.\n', matrixId, length(matrixPos));
 
 end
